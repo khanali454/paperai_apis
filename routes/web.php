0aafdeefc88afd623e0ee4de52325f56e4acb4b0
khanali454 <khanandkhan061@gmail.com>
2025-08-28 13:13:57 +0500

    add vendors

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return "Hello World!";
+});
+
+// command to run migration & seeding data 
+Route::get('/migrate', function () {
+    Artisan::call(command: 'migrate:fresh --seed');
+    return "Database migrated and seeded!";
 });
