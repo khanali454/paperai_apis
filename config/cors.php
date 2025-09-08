a2db780eb9c3ff47bf216cde7b10b546e71b2fb2
khanali454 <khanandkhan061@gmail.com>
2025-09-08 16:08:55 +0500

    cors set

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','http://apis.babalruk.com','https://apis.babalruk.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
