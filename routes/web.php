c61bb0a9c3fb19c13d922a9f735d929c72d54c35
khanali454 <khanandkhan061@gmail.com>
2025-09-09 16:09:11 +0500

    debuging...

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Hello World!";
+    return "Hello World! sir";
 });
 
 // command to run migration & seeding data 
